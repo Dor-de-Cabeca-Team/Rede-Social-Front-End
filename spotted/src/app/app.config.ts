6bac659aa57ad60f417289b872b9795f0d9f2678
Vtormacs <macskids98play@hotmail.com>
2024-10-30 08:11:36 -0300

    campo de data no register

--- spotted/src/app/app.config.ts
+++ spotted/src/app/app.config.ts
@@ -5,7 +5,8 @@ import { routes } from './app.routes';
 import { provideAnimations } from '@angular/platform-browser/animations';
 import { provideHttpClient } from '@angular/common/http';
 import { provideClientHydration } from '@angular/platform-browser';
+import { provideAnimationsAsync } from '@angular/platform-browser/animations/async';
 
 export const appConfig: ApplicationConfig = {
-  providers: [provideRouter(routes), provideAnimations(), provideHttpClient(), provideClientHydration(), provideZoneChangeDetection(), provideAnimations()]
+  providers: [provideRouter(routes), provideAnimations(), provideHttpClient(), provideClientHydration(), provideZoneChangeDetection(), provideAnimations(), provideAnimationsAsync()]
 };
--- spotted/src/app/components/layout/forms/login-register-form/login-register-form.component.ts
+++ spotted/src/app/components/layout/forms/login-register-form/login-register-form.component.ts
@@ -2,18 +2,30 @@ import { Component, inject } from '@angular/core';
 import { Router } from '@angular/router';
 import { MdbFormsModule } from 'mdb-angular-ui-kit/forms';
 import { MdbTabsModule } from 'mdb-angular-ui-kit/tabs';
+import { MatDatepickerModule } from '@angular/material/datepicker';
+import { MatInputModule } from '@angular/material/input';
+import { MatFormFieldModule } from '@angular/material/form-field';
+import { MatNativeDateModule } from '@angular/material/core';
+import { provideNativeDateAdapter } from '@angular/material/core';
 
 @Component({
   selector: 'app-login-register-form',
   standalone: true,
-  imports: [MdbFormsModule, MdbTabsModule],
+  imports: [
+    MdbFormsModule,
+    MdbTabsModule,
+    MatFormFieldModule,
+    MatInputModule,
+    MatDatepickerModule,
+    MatNativeDateModule,
+  ],
+  providers: [provideNativeDateAdapter()],
   templateUrl: './login-register-form.component.html',
-  styleUrl: './login-register-form.component.scss',
+  styleUrls: ['./login-register-form.component.scss'],
 })
 export class LoginRegisterFormComponent {
   router = inject(Router);
   logar() {
     this.router.navigate(['/principal']);
   }
-
 }
