031b1700963f5f07933fe6cf61f0c65f662769ab
Vtormacs <macskids98play@hotmail.com>
2024-10-30 15:51:18 -0300

    estrutura pagina principal

--- spotted/src/app/components/layout/principal/principal.component.ts
+++ spotted/src/app/components/layout/principal/principal.component.ts
@@ -1,14 +1,14 @@
 import { Component } from '@angular/core';
 import { FeedComponent } from '../feed/feed.component';
 import { SidebarComponent } from "../sidebar/sidebar.component";
+import { MdbScrollspyModule } from 'mdb-angular-ui-kit/scrollspy';
+import { TredingComponent } from "../treding/treding.component";
 
 @Component({
   selector: 'app-principal',
   standalone: true,
-  imports: [FeedComponent, SidebarComponent],
+  imports: [FeedComponent, SidebarComponent, MdbScrollspyModule, TredingComponent],
   templateUrl: './principal.component.html',
-  styleUrl: './principal.component.scss'
+  styleUrl: './principal.component.scss',
 })
-export class PrincipalComponent {
-
-}
+export class PrincipalComponent {}
---
+++ spotted/src/app/components/layout/treding/treding.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-treding',
+  standalone: true,
+  imports: [],
+  templateUrl: './treding.component.html',
+  styleUrl: './treding.component.scss'
+})
+export class TredingComponent {
+
+}
