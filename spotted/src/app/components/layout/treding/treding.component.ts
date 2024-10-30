0abb481493079b55878c4388652964d697772004
Vtormacs <macskids98play@hotmail.com>
2024-10-30 17:58:13 -0300

    lista de trinding imcompleto tem q criar model de tags e colocar no model do post para pegar o nome da tag

--- spotted/src/app/components/layout/treding/treding.component.ts
+++ spotted/src/app/components/layout/treding/treding.component.ts
@@ -1,12 +1,34 @@
-import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { Component, inject } from '@angular/core';
+import { PostService } from '../../../services/post/post.service';
+import { Post } from '../../../models/post/post';
 
 @Component({
   selector: 'app-treding',
   standalone: true,
-  imports: [],
+  imports: [CommonModule],
   templateUrl: './treding.component.html',
-  styleUrl: './treding.component.scss'
+  styleUrls: ['./treding.component.scss'],
 })
 export class TredingComponent {
+  postService = inject(PostService);
+  trendingList: { category: string; description: string }[] = [];
 
+  constructor() {
+    this.loadTrendingPosts();
+  }
+
+  loadTrendingPosts() {
+    this.postService.top10PostsComLike().subscribe({
+      next: (posts: Post[]) => {
+        this.trendingList = posts.map((post) => ({
+          category: post.conteudo, // Ajuste para o nome correto do campo da categoria
+          description: post.conteudo,
+        }));
+      },
+      error: (err) => {
+        console.error('Error loading trending posts: ' + err);
+      },
+    });
+  }
 }
