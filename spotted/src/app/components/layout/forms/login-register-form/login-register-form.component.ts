4cd8072a53125fe7615168d222fdc08f20473e8e
Xua1zin <jtoscanalbieri@gmail.com>
2024-11-04 10:53:05 -0300

    Date picker adjusted and form cleared when account is successfully registered

--- spotted/src/app/components/layout/forms/login-register-form/login-register-form.component.ts
+++ spotted/src/app/components/layout/forms/login-register-form/login-register-form.component.ts
@@ -41,6 +41,21 @@ export class LoginRegisterFormComponent {
 
   constructor(private userService: UserService) {}
 
+  formatDate(event: Event): void {
+    const input = event.target as HTMLInputElement;
+    let value = input.value.replace(/\D/g, ''); // Remove caracteres não numéricos
+  
+    if (value.length >= 2) {
+      value = value.slice(0, 2) + '/' + value.slice(2);
+    }
+    if (value.length >= 5) {
+      value = value.slice(0, 5) + '/' + value.slice(5);
+    }
+  
+    input.value = value;
+  }
+  
+
   onLoginSubmit(event: Event): void {
     event.preventDefault();
     this.logar(this.email, this.senha);
@@ -103,11 +118,22 @@ export class LoginRegisterFormComponent {
         this.user = user;
         alert('Usuário registrado com sucesso');
         console.log('Usuário registrado:', user);
+
+        this.limparFormulario();
       },
       (error) => {
         console.error('Erro ao registrar usuário', error);
         alert('Erro ao registrar usuário');
       }
     );
+  }
+
+  limparFormulario(){
+    this.email = '';
+    this.senha = '';
+    this.repeatPassword = '';
+    this.nome = '';
+    this.dataNascimento = new Date(); 
+    this.termosAceitos = false;
   }
 }
